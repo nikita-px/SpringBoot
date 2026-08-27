e4c6f41a5a502aeb43b4a2f0e8324a3bb1c04516
Nikita Tayade <ntayade9322@gmail.com>
2026-08-27 16:02:30 +0530

    Implement Dev class with Laptop dependency
    Added Dev class with dependency on Laptop class.

--- DemoAppApplication.java
+++ DemoAppApplication.java
@@ -21,15 +21,26 @@ public class DemoAppApplication {
 
 
 // Laptop.java
-
 package com.example1.demoApp;
-
 import org.springframework.stereotype.Component;
-
 @Component //which tell to the IOC to create the object of this class also
 public class Laptop {
     public void compile(){
         System.out.println("this is the method of the Laptop class ");
+    }
+}
+
+// Dev.class
+package com.example1.demoApp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+@Component
+public class Dev {
+@Autowired  //Dev object is dependent on the laptop then autowire help it to connected with the @Autowired Annotation ...
+    private Laptop laptop;
+    public void Build(){
+        laptop.compile();
+        System.out.println("this is the method of dev class");
     }
 }
 
