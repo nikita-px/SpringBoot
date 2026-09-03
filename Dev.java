4571cb5430ab3374ca20ca5d66bbd547cf390e48
Nikita Tayade <ntayade9322@gmail.com>
2026-09-03 16:34:23 +0530

    Add Dev class with build method

---
+++ Dev.java
@@ -0,0 +1,10 @@
+package niki.com.example.demo11;
+
+import org.springframework.stereotype.Component;
+@Component
+public class Dev {
+
+    public void build(){
+        System.out.println("this is the method of the Dev class");
+    }
+}
