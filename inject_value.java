01ebca3570044612a8e30a88b88923b941577113
Nikita Tayade <ntayade9322@gmail.com>
2026-09-04 23:37:33 +0530

    Add Spring XML configuration for beans
    Added Spring XML configuration for Dev and Laptop beans.

--- inject_value.java
+++ inject_value.java
@@ -16,3 +16,18 @@ public class Main {
 
 //Spring.xml
 
+<?xml version="1.0" encoding="UTF-8"?>
+<beans xmlns="http://www.springframework.org/schema/beans"
+       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+       xsi:schemaLocation="
+        http://www.springframework.org/schema/beans http://www.springframework.org/schema/beans/spring-beans.xsd">
+    <!-- bean definitions here -->
+    <bean id="dev" class="org.example.Dev">
+        <property name="age" value="36"/>  // where is the property is define here 
+    </bean>
+    <!--this is the class of the laptop  -->
+    <bean id="laptop" class="org.example.Laptop">
+    </bean>
+</beans>
+
+
