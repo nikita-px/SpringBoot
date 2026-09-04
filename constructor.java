9837ad6697e2b0a1025d99afea768e8e9df89202
Nikita Tayade <ntayade9322@gmail.com>
2026-09-04 23:51:22 +0530

    Add Spring XML configuration for Dev and Laptop beans
    Added Spring XML configuration for bean definitions.

--- constructor.java
+++ constructor.java
@@ -51,3 +51,29 @@ public class Dev {
         System.out.println("working project of the dev class ...");
     }
 }
+
+//Spring.xml
+
+<?xml version="1.0" encoding="UTF-8"?>
+<beans xmlns="http://www.springframework.org/schema/beans"
+       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+       xsi:schemaLocation="
+        http://www.springframework.org/schema/beans http://www.springframework.org/schema/beans/spring-beans.xsd">
+
+    <!-- bean definitions here -->
+
+
+    <bean id="dev" class="org.example.Dev">
+<!--        <property name="age" value="36"/>-->
+
+        <constructor-arg value="50"/>
+    </bean>
+
+    <!--this is the class of the laptop  -->
+
+    <bean id="laptop" class="org.example.Laptop">
+    </bean>
+
+</beans>
+
+
