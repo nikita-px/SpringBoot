d030df2f52ba846a6e8f7ba7f45aba03ed5e3b20
Nikita Tayade <ntayade9322@gmail.com>
2026-09-05 00:06:34 +0530

    Implement constructor injection in Dev class
    Added constructor injection for age and laptop in Dev class.

--- Constructor_injection_reference.java
+++ Constructor_injection_reference.java
@@ -26,5 +26,63 @@
 
   //Dev.java
 
-  
+  package org.example;
+
+public class Dev {
+
+  private Laptop laptop;
+    private int age=90; //assign the value here
+    //constructor
+
+    public Dev(){
+        System.out.println("this is the constructor of the class Dev ");
+    }
+
+    public Dev(int age) { //here is the parameterized constructor are created
+        //and it value ia assign in the spring.xml file underthe attribute the constructor-arg
+        this.age = age;
+        System.out.println("age value by the constructor injection");
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Laptop getLaptop() {
+        return laptop;
+    }
+
+    public void setLaptop(Laptop laptop) {
+        this.laptop = laptop;
+    }
+
+    //method
+    public void build(){
+        System.out.println("working project of the dev class ...");
+        laptop.compile(); //-----------> calling the method of the laptop method
+    }
+}
+
+
+// Main.java
+
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+
+        // BeanFactory ------------->
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Dev obj= (Dev) context.getBean("dev");
+    }
+}
 
