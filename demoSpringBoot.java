5a50e78f81dd14fee9df31f9abb6074b174d49da
Nikita Tayade <ntayade9322@gmail.com>
2026-09-06 14:55:05 +0530

    Add initial Spring Boot application setup

---
+++ demoSpringBoot.java
@@ -0,0 +1,13 @@
+package com.example_of_simple_web_app.demos_simple_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosSimpleWebApplication {
+
+    public static void main(String[] args) {
+       SpringApplication.run(DemosSimpleWebApplication.class, args);
+    }
+
+}
